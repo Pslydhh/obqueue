35ec9fe837575e17d296032a3f1c117b1a28e649
Pslydhh <luouyuyou@gmail.com>
2018-05-06 21:59:26 +0800

    Update obqueue_no_blocking.h

--- obqueue_no_blocking.h
+++ obqueue_no_blocking.h
@@ -182,7 +182,7 @@ void *ob_dequeue(obqueue_t *q, handle_t *th) {
 	 */
 	if((index & NBITS) == NBITS) {
 		long init_index = q->init_id;
-		if(th->pop_node->id - init_index >= q->threshold 
+		if((th->pop_node->id - init_index) >= q->threshold 
 			&& init_index >= 0
 			&& CASa(&q->init_id, &init_index, -1)) {
 			
