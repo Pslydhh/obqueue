36cdcfc91d86ceb92e2e526b78125666f24dd851
Pslydhh <luouyuyou@gmail.com>
2020-04-02 18:31:09 +0800

    Update primitives.h

--- primitives.h
+++ primitives.h
@@ -98,12 +98,12 @@ _compare_and_swap(void ** ptr, void ** expected, void * desired) {
 /* __intptr_t if P is true, or T if P is false.  */
 # define __integer_if_pointer_type_sub(T, P) \
   __typeof__ (*(0 ? (__typeof__ (0 ? (T *) 0 : (void *) (P))) 0 \
-		  : (__typeof__ (0 ? (__intptr_t *) 0 : (void *) (!(P)))) 0))
+          : (__typeof__ (0 ? (__intptr_t *) 0 : (void *) (!(P)))) 0))
 
 /* __intptr_t if EXPR has a pointer type, or the type of EXPR otherwise.  */
 # define __integer_if_pointer_type(expr) \
   __integer_if_pointer_type_sub(__typeof__ ((__typeof__ (expr)) 0), \
-				__pointer_type (__typeof__ (expr)))
+                __pointer_type (__typeof__ (expr)))
 
 /* Cast an integer or a pointer VAL to integer with proper type.  */
 # define cast_to_integer(val) ((__integer_if_pointer_type (val)) (val))
